27b774cad4457358be3d2e2cbde1d2d9e9fbe86b
chenxiao <47113131@qq.com>
2019-04-22 11:31:17 +0800

    init

--- server/server/util/ws.js
+++ server/server/util/ws.js
@@ -1,5 +1,6 @@
 var io = require('socket.io-client')
-let ws = io("ws://127.0.0.1:3000");
+var nuxtConfig = require('../../../nuxt.config')
+let ws = io("ws://127.0.0.1:" + nuxtConfig.server.port);
 module.exports = {
   getWS: function () {
     return ws
